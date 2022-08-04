5b3cd42e17453862282a58bed202e0ef0ed75a7c
Maxim <metallmuff@gmail.com>
2022-08-05 04:01:35 +1000

    объявляем массивы

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] mass = new string[6] { "8645", "I", "did", "DarkSoul", "it!", "new_line" };
+string[] mass2 = new string[mass.Length];
+void symbol_count(string[] mass, string[] mass2)
