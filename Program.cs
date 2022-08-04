e065eda307731385c245c595d817e95da1377e56
Maxim <metallmuff@gmail.com>
2022-08-05 04:04:49 +1000

    объявляем метод определения строк менее 3х символов

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿string[] mass = new string[6] { "8645", "I", "did", "DarkSoul", "it!", "new_line" };
 string[] mass2 = new string[mass.Length];
 void symbol_count(string[] mass, string[] mass2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (mass[i].Length <= 3)
+        {
+            mass2[count] = array1[i];
+            count++;
+        }
+    }
+}
+
