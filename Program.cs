b399b26a46e5339588099696f13a2aca04d45022
Maxim <metallmuff@gmail.com>
2022-08-05 04:06:22 +1000

    выводим на экран полученные данные

--- Program.cs
+++ Program.cs
@@ -3,13 +3,28 @@ string[] mass2 = new string[mass.Length];
 void symbol_count(string[] mass, string[] mass2)
 {
     int count = 0;
-    for (int i = 0; i < array1.Length; i++)
+    for (int i = 0; i < mass.Length; i++)
     {
         if (mass[i].Length <= 3)
         {
-            mass2[count] = array1[i];
+            mass2[count] = mass[i];
             count++;
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+
+}
+
+symbol_count(mass, mass2);
+
+Console.WriteLine("Полученные данные: ");
+Console.Write("~ ");
+PrintArray(mass2); 
+Console.Write("~ ");
 
